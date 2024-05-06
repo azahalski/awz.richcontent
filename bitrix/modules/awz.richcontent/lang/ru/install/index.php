90dd1b2e5e02473f17810f2bd9af79bf421431d3
Andrew Zahalski <info@zahalski.dev>
2024-05-06 11:56:55 +0300

    - изменения в языковых переменных;

--- bitrix/modules/awz.richcontent/lang/ru/install/index.php
+++ bitrix/modules/awz.richcontent/lang/ru/install/index.php
@@ -1,4 +1,4 @@
 <?
 $MESS['AWZ_RICHCONTENT_MODULE_NAME'] = "AWZ: Html в Rich Content";
 $MESS['AWZ_RICHCONTENT_MODULE_DESCRIPTION'] = "Модуль для создания разметки Rich Content";
-$MESS['AWZ_COMPANY_NAME'] = "Andrew Zahalski";
+$MESS['AWZ_PARTNER_NAME'] = "Andrew Zahalski";
