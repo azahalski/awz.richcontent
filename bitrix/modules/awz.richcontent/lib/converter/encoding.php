72d41c386e3314e20006ea5312ec158e9ece2f30
Andrew Zahalski <info@zahalski.dev>
2024-10-01 15:41:19 +0300

    - удален метод getCurrentEncoding с ядра

--- bitrix/modules/awz.richcontent/lib/converter/encoding.php
+++ bitrix/modules/awz.richcontent/lib/converter/encoding.php
@@ -11,5 +11,32 @@ class Encoding extends bxEncoding{
     public static function convertEntities($html){
         return self::convert($html,  self::getCurrentEncoding(), 'HTML-ENTITIES');
     }
+	
+	protected static function getCurrentEncoding(): string
+    {
+        $currentCharset = null;
+
+        $context = Application::getInstance()->getContext();
+        if ($context != null)
+        {
+            $culture = $context->getCulture();
+            if ($culture != null)
+            {
+                $currentCharset = $culture->getCharset();
+            }
+        }
+
+        if ($currentCharset == null)
+        {
+            $currentCharset = Configuration::getValue("default_charset");
+        }
+
+        if ($currentCharset == null)
+        {
+            $currentCharset = "Windows-1251";
+        }
+
+        return $currentCharset;
+    }
 
 }
