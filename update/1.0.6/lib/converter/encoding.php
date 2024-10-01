ac9092033ffed78bace3e34fbc6d95e8355b5b5c
Andrew Zahalski <info@zahalski.dev>
2024-10-01 15:45:42 +0300

    gulp checkpoint

---
+++ update/1.0.6/lib/converter/encoding.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Awz\RichContent\Converter;
+
+use Bitrix\Main\Text\Encoding as bxEncoding;
+
+class Encoding extends bxEncoding{
+
+    public static $defaultCharset = null;
+
+    public static function convertEntities($html){
+        return self::convert($html,  self::getCurrentEncoding(), 'HTML-ENTITIES');
+    }
+	
+	protected static function getCurrentEncoding(): string
+    {
+        $currentCharset = null;
+
+        $context = Application::getInstance()->getContext();
+        if ($context != null)
+        {
+            $culture = $context->getCulture();
+            if ($culture != null)
+            {
+                $currentCharset = $culture->getCharset();
+            }
+        }
+
+        if ($currentCharset == null)
+        {
+            $currentCharset = Configuration::getValue("default_charset");
+        }
+
+        if ($currentCharset == null)
+        {
+            $currentCharset = "Windows-1251";
+        }
+
+        return $currentCharset;
+    }
+
+}
